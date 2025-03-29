2cb6c078e852563ffda0e883dce039ee99c4b33f
steve-ababio <stevo12424@live.com>
2025-03-29 19:33:08 +0000

    fix: resolve primeng theme  Aura error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import Aura from '@primeng/themes/Aura';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
