45d5817bf2f588e99d99c5e671cb01b48616203a
steve-ababio <stevo12424@live.com>
2025-03-29 20:50:08 +0000

    fix: provide appropriate home page cta button link

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { WaterMarkTextComponent } from "../../components/water-mark-text/water-mark-text.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, WaterMarkTextComponent],
+  imports: [NavbarComponent, WaterMarkTextComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
